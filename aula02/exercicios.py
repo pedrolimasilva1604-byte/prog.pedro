9d304ef9b52c3daaeeb19aa9d7d9d781ee6224d6
pedro-giga <pedro.lima.silva1604@gmail.com>
2026-09-18 23:37:26 +0000

    Aula02 (feita)

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -6,28 +6,60 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def remove_negativos(lista):
-    """Devolve uma lista nova so com os numeros que nao sao negativos."""
-    pass
+    nova_lista = []
+
+    for i in range(len (lista)):
+        if lista[i] > 0:
+            nova_lista.append(lista[i])
+
+    return nova_lista      
 
 
 def inverte(lista):
-    """Devolve uma lista nova na ordem contraria.
-    Sem usar reverse() e sem usar [::-1]."""
-    pass
+    inicio = 0
+    fim = len(lista) - 1
+    troca = 0
 
+    while inicio < fim:
+        troca = lista[inicio]
+        lista[inicio] = lista[fim]
+        lista[fim] = troca
+        inicio += 1
+        fim -= 1
+    return lista
 
 def busca_binaria(lista, alvo):
-    """Recebe uma lista JA ORDENADA. Devolve a posicao do alvo, ou -1."""
-    pass
 
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        
+        meio = (inicio + fim) // 2
+
+        if lista[meio] == alvo:
+            return meio 
+
+        if lista[meio] < alvo:
+            inicio = meio + 1
+
+        else: fim = meio - 1
+
+    return -1
 
 def intercala(lista_a, lista_b):
-    """Devolve uma lista nova alternando os elementos das duas.
-    As duas listas tem o mesmo tamanho."""
-    pass
+    nova_lista = []
 
+    for i in range(len(lista_a)):
+        nova_lista.append(lista_a[i])
+        nova_lista.append(lista_b[i])
+
+    return nova_lista
+        
 
 def remove_repetidos(lista):
-    """(Desafio) Devolve uma lista nova sem repetidos,
-    mantendo a ordem da primeira aparicao."""
-    pass
+    nova_lista = []
+    for i in lista:
+        if i not in nova_lista:
+            nova_lista.append(i)
+    return nova_lista
